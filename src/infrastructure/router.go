07b51d9296cde24997f121668c5d5d69cfe0e913
michinoins <m1i1k2i1ya@gmail.com>
2020-12-13 22:24:29 +0900

    cors設定

--- src/infrastructure/router.go
+++ src/infrastructure/router.go
@@ -5,11 +5,15 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func Init() {
 	// Echo instance
 	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
 	userController := controllers.NewUserController(NewSqlHandler())
 
 	e.GET("/users", func(c echo.Context) error {
@@ -23,7 +27,7 @@ func Init() {
 		return c.String(http.StatusOK, "created")
 	})
 
-	//rest思想なのでpathは同じにしてメソッドで分ける
+	//rest思想なので同じパスでメソッド名を分ける
 	e.DELETE("/users/:id", func(c echo.Context) error {
 		id := c.Param("id")
 		userController.Delete(id)
